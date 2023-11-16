6c7ce063347ed18663f76ae60ede67b5e28ec9e0
danjwalton <20973540+danjwalton@users.noreply.github.com>
2023-11-16 23:59:39 +0000

    Project IPLs and poverty separately

---
+++ project_code/1. project_ipls.R
@@ -0,0 +1,83 @@
+#Install required packages
+required.packages <- c("data.table","jsonlite", "rstudioapi", "httr")
+install.packages(required.packages[!(required.packages %in% installed.packages())])
+lapply(required.packages, require, character.only=T)
+
+#Set working directory as local folder
+setwd(dirname(dirname(getActiveDocumentContext()$path)))
+
+#Set up API base address
+pip <- "https://api.worldbank.org/pip/v1/pip?"
+
+#Return year of most recent available poverty data from PIP 
+pip_call <- paste0(pip, "fill_gaps=true")
+pip_response <- suppressWarnings(rbindlist(content(GET(pip_call))))
+
+pip_most_recent <- pip_response[, .(reporting_year = max(reporting_year)), by = .(country_code, reporting_level)]
+
+#Get HHFCE growth per capita rates from World Bank
+hhfce_call <- "https://api.worldbank.org/v2/country/all/indicator/NE.CON.PRVT.PC.KD.ZG?per_page=30000&format=json"
+hhfce_response <- data.table(fromJSON(hhfce_call)[[2]])
+
+hhfce_growth <- hhfce_response[countryiso3code != "", .(country_code = countryiso3code, year = date, hhfce_growth = 1+(value/100))][order(country_code, year)]
+
+#Get GDP growth per capita projections from most recent IMF WEO
+tyear <- year(Sys.Date())
+tmonth <- month(Sys.Date())
+weo_month <- ifelse(tmonth <= 10 & tmonth >= 4, 4, 10)
+weo_year <- ifelse(tmonth < 4, tyear-1, tyear)
+weo_ver <- format(as.Date(paste("1", weo_month, weo_year, sep = "-"), "%d-%m-%Y"), "%b%Y")
+
+while(T){
+  weo_call <- paste0("https://www.imf.org/-/media/Files/Publications/WEO/WEO-Database/", weo_year, "/WEO", weo_ver ,"all.ashx")
+  weo_response <- GET(weo_call)
+  if(weo_response$headers$`content-type` == "application/vnd.ms-excel") break
+  
+  if(weo_month <= 10 & weo_month > 4){
+    weo_month <- 4
+  } else {
+    if(weo_month <= 4){
+      weo_year <- weo_year - 1
+    }
+    weo_month <- 10
+  }
+  weo_ver <- format(as.Date(paste("1", weo_month, weo_year, sep = "-"), "%d-%m-%Y"), "%b%Y")
+}
+message("Using IMF WEO version ", weo_ver, ".")
+
+weo <- content(weo_response)
+weo <- suppressWarnings(fread(rawToChar(weo[weo !='00']), na.strings=c("n/a", "--")))
+
+data_cols <- c("ISO", "WEO Subject Code", grep("^\\d{4}$", names(weo), value = T))
+weo <- melt(weo[, ..data_cols], id.vars = c("ISO", "WEO Subject Code"), variable.factor = F)
+weo[, value := as.numeric(gsub(",", "", value))]
+weo[ISO == "WBG", ISO := "PSE"]
+
+weo_gdppc <- weo[`WEO Subject Code` == "NGDPRPC"][order(ISO, variable)]
+
+weo_gdppc_growth <- weo_gdppc[, .(year = variable, gdppc_growth = value/shift(value)), by = .(country_code = ISO)]
+
+#Apply GDP growth modifiers
+weo_gdppc_growth[!(country_code %in% c("CHN", "IND")), gdppc_growth := (gdppc_growth-1)*0.87+1]
+weo_gdppc_growth[country_code == "CHN", gdppc_growth := (gdppc_growth-1)*0.72+1]
+weo_gdppc_growth[country_code == "IND", gdppc_growth := (gdppc_growth-1)*0.54+1]
+
+#Merge growth rates
+growth_rates <- merge(hhfce_growth, weo_gdppc_growth, all = T, by = c("country_code", "year"))
+growth_rates <- growth_rates[, .(growth_rate = ifelse(is.na(hhfce_growth), gdppc_growth, hhfce_growth)), by = .(country_code, year)]
+
+growth_rates[is.na(growth_rates)] <- 1
+
+growth_rates[, growth_index := cumprod(growth_rate), by = country_code]
+
+#Establish poverty line years to project
+poverty_lines <- pip_most_recent[rep(seq_len(.N), max(as.numeric(growth_rates$year))-reporting_year+1)][, year := as.character(reporting_year + seq_len(.N)-1), by = .(country_code, reporting_level)]
+
+#Calculate effective poverty lines
+poverty_lines <- merge(poverty_lines, growth_rates, by = c("country_code", "year"), all.x = T)
+poverty_lines[, poverty_line := (growth_index/growth_index[which.min(year)]), by = .(country_code, reporting_level)]
+
+#Join effective lines with existing PIP line data
+poverty_lines <- unique(rbind(pip_response[, .(country_code, reporting_level, year = reporting_year, reporting_year, poverty_line = 1)], poverty_lines[, .(country_code, reporting_level, year, reporting_year, poverty_line)])[order(country_code, reporting_level, year)])
+
+fwrite(poverty_lines, "project_data/effective_ipls.csv")
--- project_code/2. project_expoverty.R
+++ project_code/2. project_expoverty.R
@@ -6,6 +6,9 @@ lapply(required.packages, require, character.only=T)
 #Set working directory as local folder
 setwd(dirname(dirname(getActiveDocumentContext()$path)))
 
+#Choose a poverty line
+poverty_line <- 2.15
+
 ##
 ##### Start of API querying ######\
 
@@ -14,22 +17,23 @@ pip <- "https://api.worldbank.org/pip/v1/pip?"
 
 #Read in effective extreme poverty lines
 ipls <- fread("project_data/effective_ipls.csv")
+ipls <- ipls[is.na(poverty_line), poverty_line := 1]
 
-#Melt to 'long' format
-ipls <- melt(ipls, id.vars = "countrycode", variable.factor = F)
-
-#Extract the reporting year (for API use), and effective year
-ipls[, `:=` (reporting_year = substr(variable, 5, 8), effective_year = substr(variable, nchar(variable) - 3, nchar(variable)))]
+#Get current PIP data at the IPL
+pip_call <- paste0(pip, "country=all&fill_gaps=true&pov_line=", poverty_line)
+pip_current <- suppressWarnings(rbindlist(content(GET(pip_call))))
+pip_current[, year := reporting_year]
 
 #Either read or create the output file
 if(length(list.files("project_data", pattern = "^projected_expoverty[.]csv$")) == 1){
   ipl_out <- fread("project_data/projected_expoverty.csv")
 } else {
-  ipl_out <- data.table(cc = character(), effective_year = integer())
+  ipl_out <- data.table(country_code = character(), year = integer())
 }
 
-#List the ipls which are outstanding to do based on the read output file
-ipls_todo <- ipls[!(paste0(countrycode, effective_year) %in% ipl_out[, paste0(cc, effective_year)])]
+#List the ipls which are outstanding to do based on the read output file and current PIP data
+ipls_todo <- ipls[!(paste0(country_code, reporting_level, year) %in% ipl_out[, paste0(country_code, reporting_level, year)])]
+ipls_todo <- unique(ipls_todo[!(paste0(country_code, reporting_level, year)) %in% pip_current[, paste0(country_code, reporting_level, year)]])
 
 #Iterate through the outstanding ipls
 for(i in 1:nrow(ipls_todo)){
@@ -37,13 +41,13 @@ for(i in 1:nrow(ipls_todo)){
   
   #Read parameters for the API call from the list of ipls
   ipl_r <- ipls_todo[i]
-  cc <- ipl_r$countrycode
+  cc <- ipl_r$country_code
   rep_lvl <- "all"
   reporting_year <- ipl_r$reporting_year
-  pov_line <- round(ipl_r$value, 3)
+  pov_line <- round(ipl_r$poverty_line, 3)
   
   #Message country code and year to console to indicate progress
-  message(cc, ipl_r$effective_year)
+  message(cc, ipl_r$year)
   
   #Select subnational data when required
   if(nchar(cc) > 3){
@@ -59,16 +63,15 @@ for(i in 1:nrow(ipls_todo)){
   if(status_code(GET(pip_call)) != 404){
     
     #If valid, read the data from it
-    ipl_response <- rbindlist(content(GET(pip_call)))
+    ipl_response <- suppressWarnings(rbindlist(content(GET(pip_call))))
   } else {
     
     ipl_response <- data.table()
   }
   
   #Fill in the effective year and original country code to the outputted data
-  ipl_response$effective_year <- ipl_r$effective_year
-  ipl_response$cc <- ipl_r$countrycode
-  
+  ipl_response$year <- ipl_r$year
+
   #Append the outputted data to the output table and write it to the local folder
   ipl_out <- rbind(ipl_out, ipl_response, fill = T)
   fwrite(ipl_out, "project_data/projected_expoverty.csv")
@@ -81,26 +84,21 @@ for(i in 1:nrow(ipls_todo)){
 ##### Start of analysis ######
 
 #Total calculations
-extreme_pov <- ipl_out
+extreme_pov <- unique(rbind(pip_current, ipl_out))
 
-extreme_pov <- extreme_pov[country_name != ""]
-extreme_pov[, effective_year := as.integer(effective_year)]
-extreme_pov <- extreme_pov[order(cc, effective_year)]
+extreme_pov[, year := as.integer(year)]
+extreme_pov <- extreme_pov[order(country_code, reporting_level, year)]
 
-#Fill SSD with SDN pre-2008
-extreme_pov[cc == "SSD" & is.na(headcount), headcount := extreme_pov[cc == "SDN" & extreme_pov[cc == "SSD", is.na(headcount)], headcount]]
-
-#Fill other blanks with nearest value
-extreme_pov[, headcount := nafill(headcount, "nocb"), by = .(cc)]
-extreme_pov[, headcount := nafill(headcount, "locf"), by = .(cc)]
+#Remove sub-national figures for China, Indonesia, and India to avoid double counting
+extreme_pov <- extreme_pov[!((country_code %in% c("CHN", "IDN", "IND")) & reporting_level != "national")]
 
 #Read WUP population function from remote repo
 source("https://raw.githubusercontent.com/devinit/gha_automation/main/general/wupData.R")
 
 #Calculate number of poor
 wupPop <- wup_get()
-wupPop[, cc := paste0(ISO3, ifelse(area == "total", "", ifelse(area == "rural", "-R", "-U")))]
-extreme_pov <- merge(extreme_pov, wupPop[, .(cc, effective_year = as.integer(year), population)], all.x = T)
+wupPop[area == "total", area := "national"]
+extreme_pov <- merge(extreme_pov, wupPop[, .(country_code = ISO3, reporting_level = area, year = as.integer(year), population)], all.x = T)
 extreme_pov[, poor := headcount * population]
 
 #Fill gaps with regional estimates
@@ -108,14 +106,7 @@ pip_call <- paste0(pip, "country=all&year=2019&fill_gaps=true")
 pip_response <- data.table(fromJSON(pip_call))
 
 missing_pip_countries <- pip_response[!(country_code %in% extreme_pov$country_code), .(country_name, country_code, region_name, region_code, estimation_type = "regional")]
-regional_headcounts <- extreme_pov[, .(headcount = sum(poor)/sum(population)), by = .(effective_year, region_name)]
-
-missing_pip_countries <- merge(missing_pip_countries, regional_headcounts, by = c("region_name"), all.x = T, allow.cartesian = T)
-missing_pip_countries <- merge(missing_pip_countries, wupPop[, .(cc, country_code = cc, effective_year = as.integer(year), population)], all.x = T, by = c("country_code", "effective_year"))
-
-missing_pip_countries[, poor := headcount * population]
-
-extreme_pov <- rbind(extreme_pov, missing_pip_countries, fill = T)[order(effective_year, country_code)]
+regional_headcounts <- extreme_pov[, .(headcount = sum(poor)/sum(population)), by = .(year, region_name)]
 
 wb_call <- "https://api.worldbank.org/v2/country?per_page=500&format=json"
 wb_response <- data.table(fromJSON(wb_call)[[2]])
@@ -124,20 +115,19 @@ missing_wb_countries <- wb_response[region.id != "NA" & !(id %in% extreme_pov$co
 missing_wb_countries[income == "High income", region_name := "Other High Income Countries"][, income := NULL]
 
 missing_wb_countries <- merge(missing_wb_countries, regional_headcounts, by = c("region_name"), all.x = T, allow.cartesian = T)
-missing_wb_countries <- merge(missing_wb_countries, wupPop[, .(cc, country_code = cc, effective_year = as.integer(year), population)], all.x = T, by = c("country_code", "effective_year"))
+missing_wb_countries <- merge(missing_wb_countries, wupPop[area == "national", .(country_code = ISO3, year = as.integer(year), population)], all.x = T, by = c("country_code", "year"))
 
 missing_wb_countries[, poor := headcount * population]
 
-extreme_pov <- rbind(extreme_pov, missing_wb_countries, fill = T)[order(effective_year, country_code)]
+extreme_pov <- rbind(extreme_pov, missing_wb_countries, fill = T)[order(year, country_code)]
 
 #Total by year
-extreme_poor_total <- extreme_pov[!is.na(poor), .(poor = sum(poor), population = sum(population)), by = effective_year][, effective_headcount := poor/population][]
-fwrite(extreme_poor_total, "output/projected_expoverty_global.csv")
+extreme_poverty_global <- extreme_pov[!is.na(poor), .(poor = sum(poor), population = sum(population)), by = year][, effective_headcount := poor/population][]
+fwrite(extreme_poverty_global, "output/projected_expoverty_global.csv")
 
 #Total by country
-extreme_poor_cc <- extreme_pov[!is.na(poor),  .(poor = sum(poor), population = sum(population)), by = .(cc, effective_year)][, effective_headcount := poor/population][]
-extreme_poor_cc <- dcast(extreme_poor_cc, cc ~ effective_year, value.var = "poor")
-fwrite(extreme_poor_cc, "output/projected_expoor_country.csv")
+extreme_poverty_cc <- extreme_pov[!is.na(poor),  .(poor = sum(poor), population = sum(population)), by = .(country_code, year)][, effective_headcount := poor/population][]
+fwrite(extreme_poverty_cc, "output/projected_expoor_country.csv")
 
 ##### End of analysis #####
 ##
